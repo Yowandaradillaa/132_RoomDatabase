3383592ab1199eb3b8adcecd0104dfc2864bf52f
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 16:03:42 +0700

    Menambahkan tabel siswa

---
+++ app/src/main/java/com/example/myroom/room/Siswa.kt
@@ -0,0 +1,12 @@
+package com.example.myroom.room
+
+
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
