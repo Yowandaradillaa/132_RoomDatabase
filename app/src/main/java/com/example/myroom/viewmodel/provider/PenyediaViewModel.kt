c1624fe90f91d72fe95a9bb412233c6c5e1e2209
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 21:18:52 +0700

    Menambahkan code final PenyediaViewModel

---
+++ app/src/main/java/com/example/myroom/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.myroom.viewmodel.provider
+
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroom.repositori.AplikasiSiswa
+import com.example.myroom.viewmodel.EntryViewModel
+import com.example.myroom.viewmodel.HomeViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        // HAPUS BARIS INI: val aplikasiSiswa = this.aplikasiSiswa()
+
+        initializer {
+            // Pindah variabel ke sini, di mana 'this' adalah CreationExtras
+            val aplikasiSiswa = this.aplikasiSiswa()
+            HomeViewModel(
+                aplikasiSiswa.container.repositoriSiswa
+            )
+        }
+        initializer {
+            // Ulangi untuk EntryViewModel
+            val aplikasiSiswa = this.aplikasiSiswa()
+            EntryViewModel(
+                aplikasiSiswa.container.repositoriSiswa
+            )
+        }
+    }
+}
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
