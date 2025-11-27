24c37f44c462677c7472c495831e6c90c4a96a42
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 17:06:45 +0700

    Memperbaiki error

--- app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
@@ -1,19 +1,17 @@
 package com.example.myroom.repositori
 
 import android.content.Context
-import androidx.compose.ui.geometry.Offset
 import com.example.myroom.room.DataBaseSiswa
 import com.example.myroom.room.SiswaDao
 
 interface ContainerApp {
-    val repositoriSiswa : RepositoriSiswa
+    val repositoriSiswa: RepositoriSiswa
 }
 
-class ContainerDataApp(private val context: Context):
-        ContainerApp{
+class ContainerDataApp(private val context: Context) : ContainerApp {
     override val repositoriSiswa: RepositoriSiswa by lazy {
         OfflineRepositoriSiswa(
-            SiswaDao = DataBaseSiswa.getDataBase(context). SiswaDao()
+            siswaDao = DataBaseSiswa.getDataBase(context).siswaDao()
         )
     }
-        }
+}
