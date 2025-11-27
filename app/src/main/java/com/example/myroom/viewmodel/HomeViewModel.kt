947aae95418556ef555ad6173b2f08e3e3f3a7ef
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 16:40:56 +0700

    Menambahkan class home view model

---
+++ app/src/main/java/com/example/myroom/viewmodel/HomeViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.myroom.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.myroom.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5000L
+    }
+
+}
