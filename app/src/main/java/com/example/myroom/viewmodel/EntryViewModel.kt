58c4581321556cac9f3ae8768bcc65316d830e31
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 16:55:00 +0700

    Menambahkan fun detail siswa dan siswa

--- app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroom/viewmodel/EntryViewModel.kt
@@ -1,9 +1,23 @@
 package com.example.myroom.viewmodel
 
+import com.example.myroom.room.Siswa
+
 data class DetailSiswa(
     val id : Int = 0,
     val nama : String,
     val alamat : String,
     val telpon : String
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
 )
 
