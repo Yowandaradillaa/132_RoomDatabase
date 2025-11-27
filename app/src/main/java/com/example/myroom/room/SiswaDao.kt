3d351d9635d8edfbcbf1f4f17361ec823e1b62ab
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 17:03:16 +0700

    Memperbaiki error di siswa dao

--- app/src/main/java/com/example/myroom/room/SiswaDao.kt
+++ app/src/main/java/com/example/myroom/room/SiswaDao.kt
@@ -1,12 +1,15 @@
 package com.example.myroom.room
 
+import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface SiswaDao {
-    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
-    fun getAllSiswa():Flow<List<Siswa>>
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
