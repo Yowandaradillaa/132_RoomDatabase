40ab7925b3a982761014d7d23f565267c7dffbf0
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 16:36:09 +0700

    Menambahkan class offline repo

--- app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
@@ -1,9 +1,17 @@
 package com.example.myroom.repositori
 
 import com.example.myroom.room.Siswa
+import com.example.myroom.room.SiswaDao
 
 interface RepositoriSiswa{
     fun getAllSiswaStream():
             suspend fun insertSiswa(siswa: Siswa)
 }
 
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+
+): RepositoriSiswa
+override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
