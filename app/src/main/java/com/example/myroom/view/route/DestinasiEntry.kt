affcd5a897cad58901e7269ffce056123e9a7390
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 21:16:14 +0700

    Menambahkan code final Destinasi entry

---
+++ app/src/main/java/com/example/myroom/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.myroom.view.route
+
+import com.example.myroom.R
+import com.example.myroom.view.route.DestinasiNavigasi
+
+object DestinasiEntry :DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
