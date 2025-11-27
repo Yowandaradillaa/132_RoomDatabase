b3847823e352f621d7741c9d18a8653cc65e0911
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 17:04:03 +0700

    Memperbaiki error di data base siswa

--- app/src/main/java/com/example/myroom/room/DataBaseSiswa.kt
+++ app/src/main/java/com/example/myroom/room/DataBaseSiswa.kt
@@ -2,22 +2,27 @@ package com.example.myroom.room
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Siswa:class], version = 1, exportSchema = false)
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
 abstract class DataBaseSiswa : RoomDatabase() {
-    abstract fun siswaDao() : SiswaDao
+
+    abstract fun siswaDao(): SiswaDao
 
     companion object {
         @Volatile
         private var Instance: DataBaseSiswa? = null
 
-        fun getDataBase(context : Context) : DataBaseSiswa {
-            return  (Instance?: synchronized(lock = this)){
+        fun getDataBase(context: Context): DataBaseSiswa {
+            return Instance ?: synchronized(this) {
                 Room.databaseBuilder(
-                    context, klass = DataBaseSiswa::class.java,
-                    name = "siswa_database")
-                    .build().also {Instance=it}
+                    context.applicationContext,
+                    DataBaseSiswa::class.java,
+                    "siswa_database"
+                ).build().also {
+                    Instance = it
+                }
             }
         }
     }
