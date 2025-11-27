4cf7547ffb2e2564633fab2a84d3e7d3cdbafa29
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 16:22:41 +0700

    Menambahkan companion object

--- app/src/main/java/com/example/myroom/room/DataBaseSiswa.kt
+++ app/src/main/java/com/example/myroom/room/DataBaseSiswa.kt
@@ -1,9 +1,24 @@
 package com.example.myroom.room
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa:class], version = 1, exportSchema = false)
 abstract class DataBaseSiswa : RoomDatabase() {
     abstract fun siswaDao() : SiswaDao
+
+    companion object {
+        @Volatile
+        private var Instance: DataBaseSiswa? = null
+
+        fun getDataBase(context : Context) : DataBaseSiswa {
+            return  (Instance?: synchronized(lock = this)){
+                Room.databaseBuilder(
+                    context, klass = DataBaseSiswa::class.java,
+                    name = "siswa_database")
+                    .build().also {Instance=it}
+            }
+        }
+    }
 }
