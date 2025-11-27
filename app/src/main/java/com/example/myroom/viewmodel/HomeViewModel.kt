1e48286ea1fb7f8f14bbaff6266544b15186fff4
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-11-27 17:05:37 +0700

    Menambahkan import

--- app/src/main/java/com/example/myroom/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myroom/viewmodel/HomeViewModel.kt
@@ -3,7 +3,11 @@ package com.example.myroom.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myroom.repositori.RepositoriSiswa
+import com.example.myroom.room.Siswa
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     companion object {
