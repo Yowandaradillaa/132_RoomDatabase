07f0b2f6b8bf94e36630890255dbe71a9a304a82
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-12-04 16:03:39 +0700

    tambah DetailViewModel dan ambil id siswa dari SavedStateHandle

---
+++ app/src/main/java/com/example/myroom/viewmodel/DetailViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.myroom.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myroom.repositori.RepositoriSiswa
+import com.example.myroom.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa) : ViewModel(){
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+
+
+
+/**
+ * UI state for ItemDetailsScreen
+ */
+data class DetailSiswaUiState(
+    val detailSiswa: DetailSiswa = DetailSiswa()
+)
