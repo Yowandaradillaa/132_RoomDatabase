3ed925d0fb980551cc8797617a3f1fefb927a472
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-12-04 15:11:12 +0700

    tambahkan argumen idSiswa dan routeWithArgs pada DestinasiDetailSiswa

--- app/src/main/java/com/example/myroom/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/myroom/view/route/DestinasiDetailSiswa.kt
@@ -3,5 +3,8 @@ package com.example.myroom.view.route
 import com.example.myroom.R
 
 object DestinasiDetailSiswa : DestinasiNavigasi {
-
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
