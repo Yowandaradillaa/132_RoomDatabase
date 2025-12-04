4916dd798246d408f3164f2286c3e7e2fd45537a
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-12-04 20:18:52 +0700

    menambahkan update siswa

--- app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroom/repositori/RepositoriSiswa.kt
@@ -10,6 +10,7 @@ interface RepositoriSiswa {
 
     fun getSiswaStream(id : Int) : Flow<Siswa?>
     suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 
 }
 
@@ -25,5 +26,7 @@ class OfflineRepositoriSiswa(
 
     override fun getSiswaStream(id: Int):Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
 
 }
